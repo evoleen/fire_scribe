46180dc42c1b92fc4d10ddf202a6bdf8f0ee5dda
sh1l0n <jesusmanresaparres@protonmail.com>
2024-10-31 11:34:52 +0100

    Editing json, First try with json_editor_flutter (really bad user experience)

--- lib/editor/editor_dialog.dart
+++ lib/editor/editor_dialog.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:firearrow_admin_app/editor/draggable_bottom_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:json_editor_flutter/json_editor_flutter.dart';
 
 class BottomDialogExample extends StatelessWidget {
   @override
@@ -13,25 +16,11 @@ class BottomDialogExample extends StatelessWidget {
           onPressed: () {
             DraggableBottomSheet.show(
               context,
-              buildContent: (context) => IgnorePointer(
-                child: ListView(
-                  padding: EdgeInsets.all(16),
-                  children: const [
-                    Text(
-                      'This is a bottom dialog',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'You can drag to resize this dialog only from the top bar.',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    // Additional content
-                  ],
-                ),
+              buildContent: (context) => JsonEditor(
+                json: jsonEncode(patientJson),
+                onChanged: (data) {
+                  print(data);
+                },
               ),
               buildDraggableArea: (context) => EntityEditorDraggableHeader(),
             );
@@ -54,3 +43,101 @@ class EditorDialog extends StatelessWidget {
     throw UnimplementedError();
   }
 }
+
+final patientJson = {
+  "resourceType": "Patient",
+  "id": "123456",
+  "identifier": [
+    {
+      "use": "usual",
+      "type": {
+        "coding": [
+          {
+            "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
+            "code": "MR",
+            "display": "Medical Record Number"
+          }
+        ]
+      },
+      "system": "http://hospital.smarthealth.org",
+      "value": "MR123456"
+    }
+  ],
+  "active": true,
+  "name": [
+    {
+      "use": "official",
+      "family": "Doe",
+      "given": ["John", "A"]
+    },
+    {
+      "use": "nickname",
+      "given": ["Johnny"]
+    }
+  ],
+  "telecom": [
+    {"system": "phone", "value": "+1-555-555-1234", "use": "home"},
+    {"system": "email", "value": "john.doe@example.com", "use": "work"}
+  ],
+  "gender": "male",
+  "birthDate": "1985-03-15",
+  "address": [
+    {
+      "use": "home",
+      "line": ["123 Main St", "Apt 4B"],
+      "city": "Metropolis",
+      "state": "NY",
+      "postalCode": "10001",
+      "country": "USA"
+    }
+  ],
+  "maritalStatus": {
+    "coding": [
+      {
+        "system": "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus",
+        "code": "M",
+        "display": "Married"
+      }
+    ]
+  },
+  "contact": [
+    {
+      "relationship": [
+        {
+          "coding": [
+            {
+              "system": "http://terminology.hl7.org/CodeSystem/v2-0131",
+              "code": "N",
+              "display": "Next of Kin"
+            }
+          ]
+        }
+      ],
+      "name": {
+        "family": "Doe",
+        "given": ["Jane"]
+      },
+      "telecom": [
+        {"system": "phone", "value": "+1-555-555-5678", "use": "mobile"}
+      ],
+      "address": {
+        "line": ["123 Main St", "Apt 4B"],
+        "city": "Metropolis",
+        "state": "NY",
+        "postalCode": "10001",
+        "country": "USA"
+      },
+      "gender": "female"
+    }
+  ],
+  "communication": [
+    {
+      "language": {
+        "coding": [
+          {"system": "urn:ietf:bcp:47", "code": "en", "display": "English"}
+        ]
+      },
+      "preferred": true
+    }
+  ]
+};
